c2834021b1b54a6338cc79fdfa8207b1c9987cd3
FXDuke <113947184+FXDuke@users.noreply.github.com>
2023-11-06 12:43:59 -0500

    overload operations + optimizations

--- DynamicBitset.cpp
+++ DynamicBitset.cpp
@@ -10,172 +10,244 @@ class DynamicBitset {
 private:
 
     uint64_t Size;
-    vector<uint64_t> Storage;
+    uint64_t* Storage = new uint64_t[0];
 
 public:
 
-    // @Operations
-    DynamicBitset Or(DynamicBitset& Target)
+    // @Overload
+
+    bool operator==(DynamicBitset& Target)
+    {
+        if (Target.getSize() != Size)
+            return false;
+        for (int Index = 0; Index < Size; Index++)
+        {
+            if (Target.getValue(Index) != Storage[Index])
+                return false;
+        }
+        return true;
+    }
+
+    bool operator!=(DynamicBitset& Target)
+    {
+        if (Target.getSize() != Size)
+            return true;
+        for (int Index = 0; Index < Size; Index++)
+        {
+            if (Target.getValue(Index) != Storage[Index])
+                return true;
+        }
+        return false;
+    }
+
+    DynamicBitset operator|(DynamicBitset& Target)
     {
         uint64_t TSize = Target.getSize();
         DynamicBitset Result(TSize * i64);
         for (uint64_t Index = 0; Index < TSize; Index++)
-            Result.Storage[Index] = this->Storage[Index] | Target.getValue(Index);
+            Result.Storage[Index] = Storage[Index] | Target.getValue(Index);
         return Result;
     }
 
-    DynamicBitset Xor(DynamicBitset& Target)
+    DynamicBitset operator^(DynamicBitset& Target)
     {
         uint64_t TSize = Target.getSize();
         DynamicBitset Result(TSize * i64);
         for (uint64_t Index = 0; Index < TSize; Index++)
-            Result.Storage[Index] = this->Storage[Index] ^ Target.getValue(Index);
+            Result.Storage[Index] = Storage[Index] ^ Target.getValue(Index);
         return Result;
     }
 
-    DynamicBitset And(DynamicBitset& Target)
+    DynamicBitset operator&(DynamicBitset& Target)
     {
         uint64_t TSize = Target.getSize();
         DynamicBitset Result(TSize * i64);
         for (uint64_t Index = 0; Index < TSize; Index++)
-            Result.Storage[Index] = this->Storage[Index] & Target.getValue(Index);
+            Result.Storage[Index] = Storage[Index] & Target.getValue(Index);
         return Result;
     }
 
-    DynamicBitset Not()
+    DynamicBitset operator~()
     {
-        DynamicBitset Result(this->Size * i64);
-        for (uint64_t Index = 0; Index < this->Size; Index++)
-            Result.Storage[Index] = mINT64_MAX ^ this->Storage[Index];
+        DynamicBitset Result(Size * i64);
+        for (uint64_t Index = 0; Index < Size; Index++)
+            Result.Storage[Index] = mINT64_MAX ^ Storage[Index];
         return Result;
     }
 
-    void shiftLeft(int Amount)
+    DynamicBitset operator<<(int Amount)
     {
-
-        if (Amount < 0)
-            return this->shiftRight(-Amount);
+        DynamicBitset Result(Size * i64);
 
         int ShiftAmount = Amount & 63;
         int Dif = 64 - ShiftAmount;
         Amount >>= 6;
 
-        for (int Index = this->Size - 1; Index > -1; Index--)
-            this->Storage[Index] = (Index - Amount > -1) ? this->Storage[Index - Amount] : 0;
+        for (int Index = Size - 1; Index > -1; Index--)
+            Result.Storage[Index] = (Index - Amount > -1) ? Storage[Index - Amount] : 0;
 
-        if (ShiftAmount > 0)
-        {
-            for (int Index = this->Size - 1; Index > -1; Index--)
-            {
-                this->Storage[Index] <<= ShiftAmount;
-                this->Storage[Index] |= (Index - 1 > -1) ? this->Storage[Index - 1] >> Dif : 0;
-            }
-        }
+        for (int Index = Size - 1; Index > -1; Index--)
+            Result.Storage[Index] = (Result.Storage[Index] << ShiftAmount) | ((Index - 1 > -1) ? Result.Storage[Index - 1] >> Dif : 0);
+
+        return Result;
+    }
+
+    DynamicBitset operator>>(int Amount)
+    {
+        DynamicBitset Result(Size * i64);
+
+        int ShiftAmount = Amount & 63;
+        int Dif = 64 - ShiftAmount;
+        Amount >>= 6;
+
+        for (int Index = 0; Index < Size; Index++)
+            Result.Storage[Index] = (Index + Amount < Size) ? Storage[Index + Amount] : 0;
+
+        for (int Index = 0; Index < Size; Index++)
+            Result.Storage[Index] = (Result.Storage[Index] >> ShiftAmount) | ((Index + 1 < Size) ? Result.Storage[Index + 1] << Dif : 0);
+
+        return Result;
+    }
+
+    void operator<<=(int Amount)
+    {
+
+        int ShiftAmount = Amount & 63;
+        int Dif = 64 - ShiftAmount;
+        Amount >>= 6;
+
+        for (int Index = Size - 1; Index > -1; Index--)
+            Storage[Index] = (Index - Amount > -1) ? Storage[Index - Amount] : 0;
+
+        for (int Index = Size - 1; Index > -1; Index--)
+            Storage[Index] = (Storage[Index] << ShiftAmount) | ((Index - 1 > -1) ? Storage[Index - 1] >> Dif : 0);
 
     }
 
-    void shiftRight(int Amount)
+    void operator>>=(int Amount)
     {
 
-        if (Amount < 0)
-            return this->shiftLeft(-Amount);
-
-        int Remainder = Amount & 63;
-        int Dif = 64 - Remainder;
+        int ShiftAmount = Amount & 63;
+        int Dif = 64 - ShiftAmount;
         Amount >>= 6;
 
-        for (int Index = 0; Index < this->Size; Index++)
-            this->Storage[Index] = (Index+Amount < this->Size) ? this->Storage[Index + Amount] : 0;
-        
-        if (Remainder > 0)
-        {
-            for (int Index = 0; Index < this->Size; Index++)
-            {
-                this->Storage[Index] >>= Remainder;
-                this->Storage[Index] |= (Index + 1 < this->Size) ? this->Storage[Index + 1] << Dif : 0;
-            }
-        }
+        for (int Index = 0; Index < Size; Index++)
+            Storage[Index] = (Index + Amount < Size) ? Storage[Index + Amount] : 0;
+
+        for (int Index = 0; Index < Size; Index++)
+            Storage[Index] = (Storage[Index] >> ShiftAmount) | ((Index + 1 < Size) ? Storage[Index + 1] << Dif : 0);
+    }
+
+    void operator|=(DynamicBitset& Target)
+    {
+        uint64_t TSize = Target.getSize();
+        for (uint64_t Index = 0; Index < TSize; Index++)
+            Storage[Index] = Storage[Index] | Target.getValue(Index);
+    }
+
+    void operator^=(DynamicBitset& Target)
+    {
+        uint64_t TSize = Target.getSize();
+        for (uint64_t Index = 0; Index < TSize; Index++)
+            Storage[Index] = Storage[Index] ^ Target.getValue(Index);
+    }
+
+    void operator&=(DynamicBitset& Target)
+    {
+        uint64_t TSize = Target.getSize();
+        for (uint64_t Index = 0; Index < TSize; Index++)
+            Storage[Index] = Storage[Index] & Target.getValue(Index);
     }
 
     // @Read / Write
     void flip(uint64_t Position)
     {
-        this->Storage[Position >> 6] ^= 1ULL << (Position & 63);
+        Storage[Position >> 6] ^= 1ULL << (Position & 63);
     }
 
     void write(uint64_t Position, bool Value)
     {
         if (Value)
-            this->Storage[Position >> 6] |= 1ULL << (Position & 63);
+            Storage[Position >> 6] |= 1ULL << (Position & 63);
         if (!Value)
-            this->Storage[Position >> 6] &= ~(1ULL << (Position & 63));
+            Storage[Position >> 6] &= ~(1ULL << (Position & 63));
     }
 
     bool read(uint64_t Position)
     {
-        return this->Storage[Position >> 6] & (1ULL << (Position & 63));
+        return Storage[Position >> 6] & (1ULL << (Position & 63));
     }
 
     void setValue(int Position, uint64_t Value)
     {
-        this->Storage[Position] = Value;
+        Storage[Position] = Value;
     }
 
     // @Generics
     void resize(uint64_t MaskSize = i64)
     {
+        delete Storage;
+
         uint64_t FinalSize = (MaskSize >> 6) + ((MaskSize & 63) > 0);
-        this->Size = FinalSize;
-        this->Storage = vector<uint64_t>(FinalSize, 0);
+        Size = FinalSize;
+        Storage = new uint64_t[FinalSize];
     }
 
     void clear()
     {
-        this->Storage = vector<uint64_t>(this->Size, 0);
+        delete Storage;
+        Storage = new uint64_t[Size];
+    }
+
+    void free()
+    {
+        delete Storage;
+        Storage = new uint64_t[1];
     }
 
     bool zero()
     {
-        for (uint64_t Index = 0; Index < this->Size; Index++)
-            if (this->Storage[Index] > 0)
-                return true;
-        return false;
+        for (uint64_t Index = 0; Index < Size; Index++)
+            if (Storage[Index] != 0)
+                return false;
+        return true;
     }
 
     string concat()
     {
         stringstream Result;
         Result << "0x";
-        for (uint64_t Index = this->Size - 1; Index > 0; Index--)
-            Result << hex << this->Storage[Index] << "_";
-        Result << hex << this->Storage[0] << "_";
+        for (uint64_t Index = Size - 1; Index > 0; Index--)
+            Result << hex << Storage[Index] << "_";
+        Result << hex << Storage[0] << "_";
         return Result.str();
     }
 
     uint64_t getValue(uint64_t Position)
     {
-        return this->Storage[Position];
+        return Storage[Position];
     }
 
     uint64_t getSize()
     {
-        return this->Size;
+        return Size;
     }
 
     DynamicBitset clone()
     {
-        DynamicBitset Result(this->Size * i64);
-        for (uint64_t Index = 0; Index < this->Size; Index++)
-            Result.Storage[Index] = this->Storage[Index];
+        DynamicBitset Result(Size * i64);
+        for (uint64_t Index = 0; Index < Size; Index++)
+            Result.Storage[Index] = Storage[Index];
         return Result;
     }
 
     // @Constructor
     DynamicBitset(uint64_t MaskSize = i64)
     {
+        delete Storage;
         uint64_t FinalSize = (MaskSize >> 6) + ((MaskSize & 63) > 0);
-        this->Size = FinalSize;
-        this->Storage = vector<uint64_t>(FinalSize, 0);
+        Size = FinalSize;
+        Storage = new uint64_t[FinalSize];
     }
 
 };
